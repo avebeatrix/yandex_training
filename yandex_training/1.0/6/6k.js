8cddcf7f49680e257179b78682ecb13b852444f7
Beatrix_Kido <la_katalina_s@mail.ru>
2021-10-06 15:22:49 +0300

    1 6k

--- yandex_training/1.0/6/6k.js
+++ yandex_training/1.0/6/6k.js
@@ -6,28 +6,18 @@ K. Медиана объединения-2
 
 let getResult = data => {
 	let parse_ints = (line) => {
-		return line.trim().split(' ').map(input => parseInt(input));
+		return line.trim().split(' ').filter(val => val != '').map(input => parseInt(input));
 	}
-
 	let check2 = (m, checkparams) => {
 		let [curval, seq] = checkparams;
-		if (seq[m]<=curval) return false;
+		if (seq[m] <= curval) return false;
 		return true;
 	}
 
 	let check = (m, checkparams) => {
-		let [L, control_seq, second_seq] = checkparams;	
-		
-		let second_index = lLeftSearch(
-			0, L-1,
-			check2,
-			[control_seq[m], second_seq]
-		);
-		if (second_seq[second_index]>control_seq[m]){
-			second_index
-		}
-		let count = m+second_index+2;	
-			
+		let [L, control_seq, second_seq] = checkparams;
+		let second_index = lLeftSearch(0, L - 1, check2, [control_seq[m], second_seq]);
+		let count = m + second_index + 2;
 		if (count <= L) return false;
 		return true;
 	}
@@ -40,16 +30,14 @@ let getResult = data => {
 				l = m + 1;
 			}
 		}
-
 		return l;
 	}
-
 	let initSeq = (x, d, a, c, m, L) => {
 		let d_arr = [d];
 		let x_arr = [x];
 		for (let i = 1; i < L; i++) {
 			x_arr.push(x_arr[i - 1] + d_arr[i - 1]);
-			d_arr.push((d_arr[i - 1] * a + c) % m);
+			d_arr.push((((d_arr[i - 1] % m) * a % m) % m + c % m) % m);
 		}
 		return x_arr;
 	}
@@ -63,18 +51,10 @@ let getResult = data => {
 	}
 	let result = '';
 	for (let i = 0; i < N; i++) {
-		for (let j = i + 1; j < N; j++) {			
-			let left_finded = lLeftSearch(
-				0, L-1,
-				check,
-				[L, seqs[i], seqs[j]]
-			);
-			let right_finded = lLeftSearch(
-				0, L-1,
-				check,
-				[L, seqs[j], seqs[i]]
-			);
-			result += Math.min(seqs[i][left_finded], seqs[j][right_finded])+ '\n';
+		for (let j = i + 1; j < N; j++) {
+			let left_finded = lLeftSearch(0, L - 1, check, [L, seqs[i], seqs[j]]);
+			let right_finded = lLeftSearch(0, L - 1, check, [L, seqs[j], seqs[i]]);
+			result += Math.min(seqs[i][left_finded], seqs[j][right_finded]) + '\n';
 		}
 	}
 
