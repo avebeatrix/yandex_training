770b91d38e17b26c4737646629a90408b64d6b5e
Beatrix_Kido <la_katalina_s@mail.ru>
2021-10-23 19:15:58 +0300

    1 8d

---
+++ yandex_training/1.0/8/8d.js
@@ -0,0 +1,92 @@
+/* D. Обход */
+let getResult = (data) => {
+
+	let parse_ints = (line) => {
+		return line.trim().split(' ').filter(val => val != '').map(input => parseInt(input));
+	}
+
+	//remaining code stays the same
+	function trampoline(fn) {
+		return function (...args) {
+			let result = fn(...args);
+
+			while (result && typeof result === 'function') {
+				result = result();
+			}
+
+			return result;
+		};
+	}
+
+	let addNode = (val, currentNode = null) => {
+		let newnode = {
+			value: val,
+			parent: currentNode,
+			left: null,
+			right: null, 
+			visited: false
+		};
+
+		if (currentNode === null) {
+			return newnode;
+		} else {
+			if (val === currentNode.value) {
+				return false;
+			}
+			if (val < currentNode.value) {			
+				if (currentNode.left !== null) {
+					return () => addNode(val, currentNode.left);
+				} else {
+					currentNode.left = newnode;
+					return newnode;
+				}
+			} else {			
+				if (currentNode.right !== null) {
+					return () => addNode(val, currentNode.right);
+				} else {
+					currentNode.right = newnode;
+					return newnode;
+				}
+			}
+		}
+	}
+	
+	let traverse = (currentNode) => {
+		if (currentNode.left !== null && !currentNode.left.visited) {			
+			return () => traverse(currentNode.left);
+		}
+		if (!currentNode.visited){
+			result.push(currentNode.value);
+			currentNode.visited = true;
+		}		
+		if (currentNode.right !== null && !currentNode.right.visited) {				
+			return () => traverse(currentNode.right, false);										
+		}
+		if (currentNode.parent!==null){
+			return () => traverse(currentNode.parent, false);	
+		}		
+	}
+	let myTraverse = trampoline(traverse);
+	let result = [];
+	let addMyNode = trampoline(addNode);
+	let digits = [...new Set(parse_ints(data[0]))];
+	let tree = addMyNode(digits[0]);
+	for (let i = 1; i < digits.length; i++) {
+		if (digits[i] !== 0) {
+			addMyNode(digits[i], tree);
+		}
+	}
+	myTraverse(tree);
+	return result.join('\n');
+
+}
+//console.log('start: ' + Date.now());
+const fs = require('fs');
+let fileContent = fs.readFileSync("input.txt", "utf8");
+
+const data = fileContent.toString().trim().split("\n");
+
+const result = getResult(data);
+
+fs.writeFileSync("output.txt", result.toString());
+//console.log('end: ' + Date.now());
