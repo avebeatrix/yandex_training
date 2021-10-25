ed731b05f7668c9a95bd1103d0f824a63d134434
Beatrix_Kido <la_katalina_s@mail.ru>
2021-10-25 15:55:00 +0300

    1 8h

---
+++ yandex_training/1.0/8/8h.js
@@ -0,0 +1,120 @@
+/* H. АВЛ-сбалансированность */
+let getResult = (data) => {
+
+	let parse_ints = (line) => {
+		return line.trim().split(' ').filter(val => val != '').map(input => parseInt(input));
+	}
+
+	//remaining code stays the same
+	function trampoline(fn) {
+		return function (...args) {
+			let result = fn(...args);
+
+			while (result && typeof result === 'function') {
+				result = result();
+			}
+
+			return result;
+		};
+	}
+
+	let addHeight = (node) => {
+		while (node.parent !== null) {
+			node.parent.height = Math.max(node.parent.height, node.height + 1);
+			node = node.parent;
+		}
+	}
+
+	let addNode = (val, currentNode = null) => {
+		let newnode = {
+			value: val,
+			parent: currentNode,
+			left: null,
+			right: null,
+			visited: false,
+			height: 0
+		};
+
+		if (currentNode === null) {
+			return newnode;
+		} else {
+			if (val === currentNode.value) {
+				return false;
+			}
+			if (val < currentNode.value) {
+
+				if (currentNode.left !== null) {
+					return () => addNode(val, currentNode.left);
+				} else {
+					currentNode.left = newnode;
+					addHeight(newnode);
+					return newnode;
+				}
+			} else {
+
+				if (currentNode.right !== null) {
+					return () => addNode(val, currentNode.right);
+				} else {
+					currentNode.right = newnode;
+					addHeight(newnode);
+					return newnode;
+				}
+			}
+		}
+	}
+
+	let traverse = (currentNode) => {
+		let left_height = currentNode.left ? currentNode.left.height + 1 : 0;
+		let right_height = currentNode.right ? currentNode.right.height + 1 : 0;
+		let balance = currentNode.height - Math.min(left_height, right_height);
+		if (balance > 1) {
+			isAVL = false;
+		}
+		if (currentNode.left !== null) {
+			if (!currentNode.left.visited) {
+				currentNode.visited = true;
+				return () => traverse(currentNode.left);
+			}
+		}
+		if (currentNode.right !== null) {
+			if (!currentNode.right.visited) {
+				currentNode.visited = true;
+				return () => traverse(currentNode.right, false);
+			}
+		}
+		currentNode.visited = true;
+		if (currentNode.parent !== null) {
+			return () => traverse(currentNode.parent, false);
+		}
+	}
+
+	let addMyNode = trampoline(addNode);
+	let digits = [...new Set(parse_ints(data[0]))];
+	let tree = addMyNode(digits[0]);
+	for (let i = 1; i < digits.length; i++) {
+		if (digits[i] !== 0) {
+			addMyNode(digits[i], tree);
+		}
+	}
+
+	let isAVL = true;
+	let myTraverse = trampoline(traverse);
+	myTraverse(tree);
+
+	if (isAVL) {
+		return 'YES';
+	} else {
+		return 'NO';
+	}
+
+}
+//console.log('start: ' + Date.now());
+const fs = require('fs');
+let fileContent = fs.readFileSync("input.txt", "utf8");
+
+const data = fileContent.toString().trim().split("\n");
+
+const result = getResult(data);
+
+fs.writeFileSync("output.txt", result.toString());
+//console.log('end: ' + Date.now());
